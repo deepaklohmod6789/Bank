ee22ef1aeb535741daee7a191c90040ba5a4cfd2
deepaklohmod6789 <deepakgujjar6789@gmail.com>
2022-04-12 20:13:21 +0530

    Initital Commit

---
+++ contracts/Account.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.6.0;
+
+contract Account {
+    address owner;
+    uint256 balance;
+
+    constructor(address accountee) public {
+        balance = 0;
+        owner = accountee;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return balance;
+    }
+
+    function addFund(uint256 value) public {
+        balance += value;
+    }
+
+    function withdrawFund(uint256 value) public {
+        balance -= value;
+    }
+}
--- scripts/deploy_nfts.py
+++ scripts/deploy_nfts.py
@@ -1,4 +1,4 @@
-from brownie import accounts, network, config
+from brownie import NFT
 from scripts.helpful_scripts import get_account
 from web3 import Web3
 
@@ -6,4 +6,5 @@ OPENSEA_URL = "https://testnets.opensea.io/assets/{}/{}"
 
 def main():
     account= get_account()
-    print(account)
+    nft=NFT.deploy({'from':account})
+    
